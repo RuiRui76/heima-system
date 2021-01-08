3063788307b1c9310cc8302ff18ad9cfbfdde4af
RuiRui <410839220@qq.com>
2021-01-08 14:53:42 +0800

    引入element组件库

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import router from './router'
 
 // 引入全局样式
 import './styles/index.less'
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+// 加载 element 组件库样式
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+// 全局注册 element 组件库
+Vue.use(ElementUI)
 
 new Vue({
   router,
