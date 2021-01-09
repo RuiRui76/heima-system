3680682e539da102d01a37628f429b1f66cb277e
RuiRui <410839220@qq.com>
2021-01-09 13:41:48 +0800

    用户登录模块

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// axios()
+// axios.get()
+// axios.post()
+
+// 创建 axios 实例
+const request = axios.crrate({
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+// 'http://api-toutiao-web.itheima.net/
+
+// 请求拦截
+
+// 响应拦截
+
+// 导出请求方法
+export default request
+
+// 谁调用谁就加载该模块
+// import request from 'request.js'
+// request.xxx// 请求类型
+// request({
+//     method: ,
+//     url: ''
+// })
